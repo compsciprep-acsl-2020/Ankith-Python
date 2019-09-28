259167c21beaf0d2388223505a7c4140f033ea92
gust <bhogavalliankith@gmail.com>
2019-09-28 14:26:08 -0400

    class 9-28 commit

---
+++ python 2.py
@@ -0,0 +1,12 @@
+a = int(input(" Enter first number"))
+b = int(input("Enter second number"))
+if(a > b):
+    print("true")
+else:
+    print("False")
+
+# > greater than
+# < less than
+# <= less than or equal to
+# >= greater than or equal to
+# = assigment 
